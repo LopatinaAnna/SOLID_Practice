9a6e54c41f78b4678f4ed1e2e3e1330939cbc1b8
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-12 20:08:33 +0200

    Add violation of SRP

---
+++ SRP Incorrect/Program.cs
@@ -0,0 +1,19 @@
+﻿using SOLID_Practice.Entities;
+
+namespace SRP_Incorrect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car
+            {
+                Model = "BMW X5",
+                Speed = 100,
+                CurrentFuel = 250
+            };
+
+            car.ShowCurrentState();
+        }
+    }
+}
