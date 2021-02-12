82256e5e11dfe4be3a7026a421df08cced6cba9e
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-12 20:20:40 +0200

    Following the SRP

---
+++ SRP Correct/Entities/TransportStateViewer.cs
@@ -0,0 +1,11 @@
+﻿using SOLID_Practice.Entities;
+using System;
+
+namespace SRP_Correct.Entities
+{
+    public class TransportStateViewer
+    {
+        public void ShowCurrentState(Transport transport)
+            => Console.WriteLine($"Speed: {transport.Speed}, fuel: {transport.CurrentFuel}");
+    }
+}
---
+++ SRP Correct/Interfaces/ITransport.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID_Practice.Interfaces
+{
+    public interface ITransport
+    {
+        void Move();
+
+        void Park();
+
+        void Refuel();
+    }
+}
