0792d6ad9fed47a5211c76b4866c651babba61aa
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-15 16:37:03 +0200

    Incorrect example for LSP

---
+++ LSP Incorrect/Entities/Bicycle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SOLID_Practice.Entities
+{
+    class Bicycle : Transport
+    {
+        public override void Repair() => Console.WriteLine("Car repair");
+
+        public override void Refuel() => throw new Exception();
+    }
+}
---
+++ LSP Incorrect/Program.cs
@@ -0,0 +1,27 @@
+﻿using SOLID_Practice.Entities;
+
+namespace LSP_Incorrect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transports = new Transport[] 
+            { 
+                new Car(),
+                new Plane(),
+                new Bicycle()
+            };
+
+            RefuelAllTransport(transports);
+        }
+
+        static void RefuelAllTransport(Transport[] transports)
+        {
+            foreach (var transport in transports)
+            {
+                transport.Refuel(); // Exception on Bicycle.Refuel()
+            }
+        }
+    }
+}
