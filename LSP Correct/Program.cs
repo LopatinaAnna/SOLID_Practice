eaf57ca226e3d222acbced67faed51beef82e3ce
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-15 16:57:00 +0200

    Сorrect example for LSP

---
+++ LSP Correct/Program.cs
@@ -0,0 +1,27 @@
+﻿using SOLID_Practice.Entities;
+using SOLID_Practice.Interfaces;
+
+namespace LSP_Correct
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var transports = new IRefuel[]
+            {
+                new Car(),
+                new Plane()
+            };
+
+            RefuelAllTransport(transports);
+        }
+
+        private static void RefuelAllTransport(IRefuel[] transports)
+        {
+            foreach (var transport in transports)
+            {
+                transport.Refuel();
+            }
+        }
+    }
+}
