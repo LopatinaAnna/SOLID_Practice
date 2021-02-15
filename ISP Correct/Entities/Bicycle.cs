8984be416ba5022149ba4b879cf5d7acdbeb28e8
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-15 17:10:22 +0200

    Correct example for ISP

---
+++ ISP Correct/Entities/Bicycle.cs
@@ -0,0 +1,18 @@
+﻿using SOLID_Practice.Interfaces;
+using System;
+
+namespace SOLID_Practice.Entities
+{
+    internal class Bicycle : IMove, IPark, IRepair
+    {
+        public string Model { get; set; }
+
+        public int Speed { get; set; }
+
+        public void Move() => Console.WriteLine("Bicycle move");
+
+        public void Park() => Console.WriteLine("Bicycle park");
+
+        public void Repair() => Console.WriteLine("Bicycle repair");
+    }
+}
---
+++ ISP Correct/Program.cs
@@ -0,0 +1,27 @@
+﻿using SOLID_Practice.Entities;
+using SOLID_Practice.Interfaces;
+
+namespace ISP_Correct
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var transports = new IRefuel[]
+            {
+                new Car(),
+                new Plane()
+            };
+
+            RefuelAllTransport(transports);
+        }
+
+        private static void RefuelAllTransport(IRefuel[] transports)
+        {
+            foreach (var transport in transports)
+            {
+                transport.Refuel();
+            }
+        }
+    }
+}
